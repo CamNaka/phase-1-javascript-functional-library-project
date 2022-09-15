72b9113dee58f87e42ffc4d85ed402c6e2b5af57
Cameron <cameronnakaba4@gmail.com>
2022-09-15 10:38:40 -0700

    functional library

---
+++ index.js
@@ -0,0 +1,81 @@
+function myEach(collection, callback) {
+  for(let ele in collection) {
+    callback(collection[ele])
+  }
+  return collection
+}
+
+function myMap(collection, callback) {  //breakdown of how .map() works
+  let modifiedArr = []
+
+  for(let ele in collection) {
+    modifiedArr.push(callback(collection[ele]))
+  }
+  return modifiedArr;
+}
+
+function myReduce(collection, callback, acc) {  //breakdown of how .reduce() works
+  let copiedArr = [...collection]
+  acc = acc || copiedArr.shift();
+  for(let ele in copiedArr) {
+    acc = callback(acc, copiedArr[ele], copiedArr)
+  }
+  return acc;
+}
+
+function myFind(collection, callback) { //breakdown of how .find() works
+  for(let ele in collection) {
+    let element = collection[ele];
+    if (callback(element)) {
+      return element
+    }
+  }
+
+}
+
+function myFilter(collection, callback) { //breakdown of how .filter() works
+  let selectedArray = [];
+
+  for(let ele in collection) {
+    let element = collection[ele];
+    if (callback(element)) {
+      selectedArray.push(element)
+    }
+  }
+  return selectedArray;
+}
+
+function mySize(collection) {
+return Object.keys(collection).length
+}
+
+function myFirst(collection, n = 1) {
+  let count = 1;
+  let array = [];
+
+  for(let ele in collection) {
+    if (count <= n) {
+      array.push(collection[ele]);
+      count++
+    }
+  }
+  return array.length > 1 ? array : array[0]
+}
+
+
+function myLast(collection, n = 1) {
+  let startingIndex = collection.length - n;
+
+  return n > 1 ? collection.slice(startingIndex, collection.length) : collection[startingIndex];
+  
+}
+
+function myKeys(collection) {
+  let keyCollect = collection
+  return Object.keys(keyCollect)
+}
+
+function myValues(collection) {
+  let valueCollect = collection
+  return Object.values(valueCollect)
+}
